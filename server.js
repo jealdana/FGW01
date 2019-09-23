242624830afbbc8615a652e0aa725936f0d348c2
Enrique Aldana <aldana.sigona@gmail.com>
2019-09-23 13:47:19 +0300

    missing ng build before sending to heroku

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
+app.use(express.static(__dirname + '/dist/FGW01'));
+app.get('/*', function(req,res){
+res.sendFile(path.join(__dirname, 'dist','FGW01', 'index.html'))
 });
-app.listen(process.env.PORT || 8080); 
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
